6d62572ec7bc49b9935cfca35f25752406a8a1ff
Debasish Mandal <debasishm89@gmail.com>
2021-11-24 11:50:00 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,30 @@
+some sample secret for testing.
+
+api key = testing
+
+secret password
+
+password : my test password dont share it with any one lol :) 
+
+API_KEY = 'SUPER SECRET API KEY'
+
+catch me if you can.
+
+
+AKIAIOSFODNN7EXAMPLE
+
+
+3st.pvt
+
+fds.pvt
+
+wfm.pvt 
+
+ghw.pvt
+
+dhe.pvt
+
+
+afds.pvt
+
+wfm.local
